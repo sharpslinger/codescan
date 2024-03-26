50416fb7ae869830aadaa075a382fa3ce56e837e
Ty Esser <sharpslinger@users.noreply.github.com>
2024-03-25 20:49:54 -0500

    make code worse

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
 ﻿// This is an example of terrible code to test scanning capability
 
-Console.WriteLine("Hello, World!");
-
-var i = "potato";
-
-if (i == "poTAHTO" || Convert.ToUInt64(i) == 4)
-{
+Console.WriteLine("Hello, World!");var i = "potato";if (i == "poTAHTO" || Convert.ToUInt64(i) == 4) { 
     Console.WriteLine("yay!");
 }
 else Console.WriteLine("aww");

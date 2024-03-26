3246db077e522c8377682e77842f1e9c08469ddb
Ty Esser <sharpslinger@users.noreply.github.com>
2024-03-25 20:02:43 -0500

    update

---
+++ CodeScan/Program.cs
@@ -0,0 +1,11 @@
+﻿// This is an example of terrible code to test scanning capability
+
+Console.WriteLine("Hello, World!");
+
+var i = "potato";
+
+if (i == "poTAHTO" || Convert.ToUInt64(i) == 4)
+{
+    Console.WriteLine("yay!");
+}
+else Console.WriteLine("aww");

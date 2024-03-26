944b75e7aef4e7ad890dc65e83d0de39ed0917de
Ty Esser <sharpslinger@users.noreply.github.com>
2024-03-25 20:55:14 -0500

    worse code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿// This is an example of terrible code to test scanning capability
 
+bool emptyVar;
 Console.WriteLine("Hello, World!");var i = "potato";if (i == "poTAHTO" || Convert.ToUInt64(i) == 4) { 
     Console.WriteLine("yay!");
 }
 else Console.WriteLine("aww");
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+throw new Exception("oops!");
